444433173041f1859bba8a6d5b10ed74d873a092
subin <61944367+Subiiiin@users.noreply.github.com>
2022-04-04 15:50:57 +0900

    Create 국민대안전교육스킵코드.js

---
+++ 국민대안전교육스킵코드.js
@@ -0,0 +1,19 @@
+var currentPage = nowPageNum;
+function goNextPage() {
+if (currentPage <= totalPageNum) {
+opener.PageMove(currentPage);
+console.log(`${currentPage} 페이지를 수강완료했습니다.`);
+currentPage = 1 + currentPage;
+goNextPage();
+} else {
+opener.PageMove(currentPage);
+alert('강의 수강이 완료되었습니다!');
+}
+}
+function runKmuMacro() {
+console.log(`현재 ${currentPage} 페이지를 수강중입니다.`);
+setTimeout(function () {
+goNextPage();
+}, 1000)
+}
+runKmuMacro();
